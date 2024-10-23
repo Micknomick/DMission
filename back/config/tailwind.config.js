8a8416ebba539ca3ed8176e4fc9cc9c43517dd08
Takayoshi Mitsuno <takayoshimitsuno@TakayoshinoMacBook-Pro.local>
2024-10-23 11:03:13 +0900

    new:トップページの作成

--- back/config/tailwind.config.js
+++ back/config/tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        '10xl': '10rem',   // 10xlは10rem（非常に大きいフォントサイズ）
+        '12xl': '12rem',   // 12xlは12rem
+        '15xl': '15rem',   // 15xlは15rem（さらに大きく）
+      },
     },
   },
   plugins: [
