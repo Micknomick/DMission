b72e656d05fb11268ed8f7ca2fa3c3bf1ae2f9cf
Takayoshi Mitsuno <takayoshimitsuno@TakayoshinoMacBook-Pro.local>
2024-11-12 07:04:10 +0900

    feat:ハンバーガーメニュー実装

--- back/app/javascript/application.js
+++ back/app/javascript/application.js
@@ -4,27 +4,31 @@ import "controllers"
 import Rails from "@rails/ujs"
 Rails.start()
 
+import "hamburger_menu"
+
 
 // ダッシュボードタスク作成用
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbo:load', () => {
   const modal = document.getElementById('task-modal');
   const openButton = document.getElementById('open-modal');
   const closeButton = document.getElementById('close-modal');
 
-  // モーダルを開く
-  openButton.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-  });
+  if (modal && openButton && closeButton) {
+    // モーダルを開く
+    openButton.addEventListener('click', () => {
+      modal.classList.remove('hidden');
+    });
 
-  // モーダルを閉じる
-  closeButton.addEventListener('click', () => {
-    modal.classList.add('hidden');
-  });
-
-  // モーダルの外をクリックして閉じる
-  window.addEventListener('click', (event) => {
-    if (event.target === modal) {
+    // モーダルを閉じる
+    closeButton.addEventListener('click', () => {
       modal.classList.add('hidden');
-    }
-  });
+    });
+
+    // モーダルの外をクリックして閉じる
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        modal.classList.add('hidden');
+      }
+    });
+  }
 });
