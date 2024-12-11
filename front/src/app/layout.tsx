5555cb6d63ada2a7b05a9cdaa2e1ea3bf9a0c13b
micknomick <ronaldo7.mt29@gmail.com>
2024-12-11 23:32:17 +0900

    edit:page.tsx

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
