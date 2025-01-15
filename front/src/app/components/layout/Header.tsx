8230d1c68a29f23ea513461ab9251e0bd655eb01
micknomick <ronaldo7.mt29@gmail.com>
2025-01-15 21:42:27 +0900

    edit:navbar.tsx

--- front/src/app/components/layout/Header.tsx
+++ front/src/app/components/layout/Header.tsx
@@ -1,33 +1,37 @@
-'use client'
-import styles from '@/app/styles/header.module.scss'
+'use client';
+
+import styles from '@/app/styles/header.module.scss';
 import { useEffect, useState } from 'react';
 import NavBar from '@/app/components/layout/nav/NavBar';
 import { AnimatePresence } from 'framer-motion';
 import { usePathname } from 'next/navigation';
 
-export default function Home() {
-
+export default function Header() {
   const [isActive, setIsActive] = useState(false);
   const pathname = usePathname();
 
-  useEffect( () => {
-    if(isActive) setIsActive(false)
-  }, [pathname])
+  // NavBar の状態を切り替える関数
+  const toggleNavBar = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    // パスが変わったら NavBar を閉じる
+    setIsActive(false);
+  }, [pathname]);
 
   return (
     <>
-    <div className={styles.main}>
-
-      <div className={styles.header}>
-        <div onClick={() => {setIsActive(!isActive)}} className={styles.button}>
-          <div className={`${styles.burger} ${isActive ? styles.burgerActive : ""}`}></div>
+      <div className={styles.main}>
+        <div className={styles.header}>
+          <div onClick={toggleNavBar} className={styles.button}>
+            <div className={`${styles.burger} ${isActive ? styles.burgerActive : ''}`}></div>
+          </div>
         </div>
       </div>
-
-    </div>
-    <AnimatePresence mode="wait">
-      {isActive && <NavBar />}
-    </AnimatePresence>
+      <AnimatePresence mode="wait">
+        {isActive && <NavBar />}
+      </AnimatePresence>
     </>
-  )
+  );
 }
--- front/src/app/components/layout/nav/NavBar.tsx
+++ front/src/app/components/layout/nav/NavBar.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+'use client';
+
+import React, { useState } from 'react';
 import styles from '@/app/styles/navbar.module.scss';
 import { motion } from 'framer-motion';
 import { usePathname } from 'next/navigation';
@@ -6,46 +8,58 @@ import { menuSlide } from '@/lib/anim';
 import Link from '@/app/components/layout/nav/Link';
 import Curve from '@/app/components/layout/nav/Curve';
 
-
 const navItems = [
   {
-    title: "DashBoard",
-    href: "/dashboard",
+    title: 'DashBoard',
+    href: '/dashboard',
   },
   {
-    title: "Misison",
-    href: "/missions/new",
+    title: 'Mission',
+    href: '/missions/new',
   },
   {
-    title: "Tasks",
-    href: "/tasks/index",
+    title: 'Tasks',
+    href: '/tasks/index',
   },
   {
-    title: "Contact",
-    href: "/contact",
+    title: 'Contact',
+    href: '/contact',
   },
-]
+];
 
-export default function index() {
-
+export default function NavBar() {
   const pathname = usePathname();
   const [selectedIndicator, setSelectedIndicator] = useState(pathname);
 
   return (
-    <motion.div variants={menuSlide} initial="initial" animate="enter" exit="exit" className={styles.menu}>
+    <motion.div
+      variants={menuSlide}
+      initial="initial"
+      animate="enter"
+      exit="exit"
+      className={styles.menu}
+    >
       <div className={styles.body}>
-        <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
-                <div className={styles.header}>
-                    <p>Navigation</p>
-                </div>
-                {
-                  navItems.map( (data, index) => {
-                    return <Link key={index} data={{...data, index}} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator}></Link>
-                  })
-                }
-        </div>
+        <nav
+          onMouseLeave={() => {
+            setSelectedIndicator(pathname);
+          }}
+          className={styles.nav}
+        >
+          <div className={styles.header}>
+            <p>Navigation</p>
+          </div>
+          {navItems.map((data, index) => (
+            <Link
+              key={index}
+              data={{ ...data, index }}
+              isActive={selectedIndicator === data.href}
+              setSelectedIndicator={setSelectedIndicator}
+            />
+          ))}
+        </nav>
       </div>
       <Curve />
     </motion.div>
-  )
+  );
 }
